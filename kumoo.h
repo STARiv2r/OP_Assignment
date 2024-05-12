26e3812caef3f8c845fd45dfabec1506df2f04a9
STARiv2r <64356856+STARiv2r@users.noreply.github.com>
2024-05-12 11:01:58 +0900

    Add files via upload

--- kumoo.h
+++ kumoo.h
@@ -18,6 +18,7 @@ int *pmfree;
 int *swfree;
 int n;
 
+
 struct pcb{
 	unsigned short pid;
 	FILE *fd;
@@ -27,9 +28,10 @@ struct pcb{
 	/* Add more fields as needed */
 };
 
-
+int ku_traverse(unsigned short va, int write);
 
 void ku_freelist_init(){
+	printf("in freelist init\n");
 	/* Initializes the free list for pfnum frames in the physical memory
 	 • Initializes the free list for sfnum frames in the swap space
 	 • Either linked list, array, or bitmap is fine
@@ -37,6 +39,13 @@ void ku_freelist_init(){
 
 	pmfree = (int*)malloc(pfnum*sizeof(int));
 	swfree = (int*)malloc(sfnum*sizeof(int));
+
+	for(int i= 0; i< 64<<12; i++){
+		pmem[i] = 0;
+	}
+	for(int i= 0; i< 64<<14; i++){
+		swaps[i] = 0;
+	}
 
 	for(int i= 0; i<pfnum; i++){
 		pmem[i] = 0;
@@ -59,17 +68,54 @@ int ku_proc_init(int argc, char *argv[]){
 	 */
 //page directory는 page fault에서 제외하고
 
-	n= argc/2;
-	myPcb = (pcb *)malloc(sizeof(struct pcb)*n);
+	printf("check");
+	printf("in proc init and n: %d\n", n);
+	printf("check");
+	printf("%d\n", argc);
+	printf("checka");
+	for(int i= 0; i < argc; i++) {
+		printf(" %s ", argv[i]);
+	}
+	FILE *input =fopen(argv[1], "r");
+	myPcb = (struct pcb *)malloc(sizeof(struct pcb)*n);
 	pdbr = (unsigned short *)malloc(sizeof(unsigned short)*n);
 	page_cursor = 0;
-
+	char com; //d
+	int ind=0;
+	int pnum;
+	char* proc[10];
+	while(!feof(input)) {
+		proc[ind] = (char *)malloc(sizeof(char)*10);
+		fscanf(input, " %d %s", &pnum, proc[ind]);
+		ind++;
+		printf(" proc: %s ", proc[ind]);
+		char str[100];
+		fgets(str, 100, input);
+		printf("%s", str);
+	}
+	n = pnum;
 	for(int i=0; i < n; i++){//argc = 7
-		myPcb[i].fd = fopen(argv[2*(i+1)], "r");
+		myPcb[i].fd = fopen(proc[i], "r");
+		printf("check 1\n");
+		if(fscanf(myPcb[i].fd, " %c", &com)==EOF) {
+			return 1;
+		}
+		printf("check 2\n");
+		if (fscanf(myPcb[i].fd, "%d %d", &myPcb[i].vbase, &myPcb[i].vlength) ==EOF) {
+			return 1;
+		}
+		printf("check 3\n");
 		myPcb[i].pid = i;
 		myPcb[i].pgdir = (unsigned short *)calloc(32, sizeof(unsigned short));
 		pdbr[i] = 0x0000;
+		printf("check 4\n");
 	}
+	//ku_dump_pmem();
+	for(int i=0; i < n; i++){
+		free(proc[i]);
+	}
+	printf("proc init end successful: %d\n", n);
+	return 0;
 
 }
 int ku_scheduler(unsigned short arg1){/*
@@ -81,10 +127,14 @@ int ku_scheduler(unsigned short arg1){/*
 	 » Updates current and pdbr
 	 */
 //pid 10이 들어오면 0번부터 시작
+	printf("in scheduler\n");
+	int pid = arg1;
+	if(arg1 == 10) pid = 0;
+
 	if(pid < 0 || pid >=n) return 1;
 
-	current = &myPcb[arg1 % n];
-	pdbr[arg1 % n] = (unsigned short)current->pgdir;
+	current = &myPcb[pid+1];
+	pdbr[pid] = (unsigned short)current->pgdir;
 	return 0;
 }
 int ku_pgfault_handler(unsigned short arg1){
@@ -99,13 +149,18 @@ int ku_pgfault_handler(unsigned short arg1){
 	 » Updates free lists
 	 » Updates PED or PTE
 * */
-	unsigned short vpn, pfn;
+	printf("in pgfault handler\n");
+	unsigned short va, pa;
 	int pid = current->pid;
-	vpn = (arg1 & 0xFFC0) >> 6;
-	pfn = vpn % pfnum;
-	if(pmfree[pfn]==0) {
-		pmfree[pfn] = 1;
-		myPcb[pid].pgdir[vpn] = pfn;
+	va = (arg1 & 0xFFC0) >> 6;
+	pa = ku_traverse(va, 0);
+
+	if(va<myPcb[pid].vbase || va>myPcb[pid].vbase+myPcb[pid].vlength) {
+		return 1;
+	}
+	if(pmfree[pa]==0) {
+		pmfree[pa] = 1;
+		myPcb[pid].pgdir[va] = pa;
 	}
 	else {//swap out
 
@@ -123,6 +178,7 @@ int ku_proc_exit(unsigned short arg1){
 	lists
 *
 * */
+	printf("in proc exit");
 	int pid = arg1;
 
 	if(pid < 0 || pid >=n) return 1; //invalid PID
@@ -137,7 +193,7 @@ int ku_proc_exit(unsigned short arg1){
 		}
 	}
 	free(myPcb[pid].pgdir);
-	free(myPcb[pid]);
+
 	return 0;
 
 }
